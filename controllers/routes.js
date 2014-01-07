15ad22ad0bc54e0647ac945dc7d1ae9f47375686
Heidi Sommer <hsommer@familysearch.org>
2014-01-07 15:23:04 -0700

    update

--- controllers/routes.js
+++ controllers/routes.js
@@ -6,10 +6,26 @@ module.exports = function(app) {
   });
   */
   
+  app.get('/page2', function(req, res){
+    res.render("page2",{
+      mymsg:"Hello World!", 
+      mysign:"Aries", 
+      mycontact:{first:"John",last:"Smith", cell:"123.456.7890"},
+      mychildren:["Betty", "Bob", "Billy", "Brenda"]
+      });
+
+  });
+
+  app.get('/name', function(req, res){
+    res.render("name",{name:"Heidi"});
+
+  });
+
   app.get('/:page', function(req, res){
     res.render(req.params.page, {});
   });
+
   app.get('/', function(req, res){
-    res.render("index", {});
+    res.render("index", {mymsg:"Hello World!"});
   });
 }
